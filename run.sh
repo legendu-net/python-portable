07273ae6be348c3ef32691f64d03fe604a7baf6b
Ben Chuanlong Du <dclong@users.noreply.github.com>
2020-07-21 00:15:15 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+docker run -it \
+    --hostname python-portable \
+    --log-opt max-size=50m \
+    -e DOCKER_USER=$(id -un) \
+    -e DOCKER_USER_ID=$(id -u) \
+    -e DOCKER_PASSWORD=$(id -un) \
+    -e DOCKER_GROUP_ID=$(id -g) \
+    -v "$(pwd)":/workdir \
+    -v "$(dirname $HOME)":/home_host \
+    dclong/python-portable /scripts/sys/init.sh -u /workdir/$1
