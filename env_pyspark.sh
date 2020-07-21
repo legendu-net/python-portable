8eb874bb97a7cfa428d9d5236bf5d87cb4a94c1b
Ben Chuanlong Du <legendu.net@outlook.com>
2020-07-21 00:49:08 -0700

    update scripts

--- env_pyspark.sh
+++ env_pyspark.sh
@@ -5,9 +5,7 @@ file=/workdir/${1:-env_pyspark_ai}.tar.gz
 /opt/python/bin/python3 -m pip install \
     findspark \
     loguru notifiers \
-    datacompy \
-    torch==1.5.1+cpu torchvision==0.6.1+cpu -f https://download.pytorch.org/whl/torch_stable.html \
-    transformers
+    datacompy
     
 cd /opt/python/
 tar -zcvf $file ./
