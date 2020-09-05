df67b2aef3c3c9fef1ca3abba466ac9d8e884c92
Ben Chuanlong Du <dclong@users.noreply.github.com>
2020-09-05 10:30:50 -0700

    Update env_pyspark_torch.sh

--- env_pyspark_torch.sh
+++ env_pyspark_torch.sh
@@ -4,7 +4,7 @@ file=/workdir/${1:-env_pyspark_torch}.tar.gz
 
 /opt/python/bin/python3 -m pip install \
     loguru notifiers \
-    datacompy \
+    pyarrow datacompy \
     torch==1.5.1+cpu torchvision==0.6.1+cpu -f https://download.pytorch.org/whl/torch_stable.html \
     transformers
 
