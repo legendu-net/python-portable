2c438419c154e16b14c78813402da611bd901130
Benjamin Du <longendu@yahoo.com>
2020-09-05 10:36:12 -0700

    add tensorflow env

---
+++ env_pyspark_tf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+file=/workdir/${1:-env_pyspark_tf}.tar.gz
+
+/opt/python/bin/python3 -m pip install \
+    loguru notifiers \
+    pandas pyarrow \
+    tensorflow "pillow==7.1.2"
+
+if [[ $? -eq 0 ]]; then
+    cd /opt/python/
+    tar -zcvf $file ./
+    chown $DOCKER_USER_ID:$DOCKER_GROUP_ID $file
+fi
