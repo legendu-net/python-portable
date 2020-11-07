8aab6f2851fedc88fcc526d7787719c663e1653a
Ben Chuanlong Du <legendu.net@outlook.com>
2020-11-07 12:34:35 -0800

    fix issues in env_python.sh

--- env_python.sh
+++ env_python.sh
@@ -3,25 +3,15 @@
 file=/workdir/${1:-env_python}.tar.gz
 
 /opt/python/bin/python3 -m pip install \
-    IPython "jupyterlab<2.3.0" \
-    loguru notifiers \
+    IPython "jupyterlab<2.3.0" nbconvert nbformat \
     pandas pyarrow \
+    tqdm GitPython toml PyYAML \
+    loguru notifiers \
+    dateparser \
+    requests \
+    pylint yapf \
+    opencv-python pillow PyPDF2 \
     git+https://github.com/dclong/xinstall@master
-    dateparser
-    pandas = ">=0.22.0"
-    loguru = ">=0.3.2"
-    toml = ">=0.10.0"
-    pylint = ">=2.4.0"
-    yapf = ">=0.28.0"
-    PyPDF2 = ">=1.26.0"
-    requests = ">=2.20.0"
-    tqdm = ">=4.40.0"
-    opencv-python = ">=4.0.0.0"
-    pillow = ">=7.0.0"
-    GitPython = ">=3.0.0"
-    nbformat = ">=5.0.7"
-    notifiers = ">=1.2.1"
-    nbconvert = ">=6.0.7"
 
 /opt/python/bin/python3 -m pip install --no-deps git+https://github.com/dclong/dsutil@master
     
