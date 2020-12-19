2a12aa5fa56f30cf0b75f0104cc0769b10869a5e
Benjamin Du <longendu@yahoo.com>
2020-12-18 21:56:34 -0800

    update env_python.sh

--- env_python.sh
+++ env_python.sh
@@ -3,19 +3,12 @@
 file=/workdir/${1:-env_python}.tar.gz
 
 /opt/python/bin/python3 -m pip install \
+    pylint yapf pytype \
     IPython "jupyterlab<2.3.0" nbconvert nbformat \
     pandas pandas-profiling pyarrow findspark \
-    JPype1 sqlparse \
-    tqdm GitPython toml PyYAML python-magic \
-    loguru notifiers \
-    dateparser \
-    requests \
-    pylint yapf \
-    opencv-python pillow PyPDF2 \
-    git+https://github.com/dclong/xinstall@main
+    git+https://github.com/dclong/xinstall@main \
+    git+https://github.com/dclong/dsutil@main
 
-/opt/python/bin/python3 -m pip install --no-deps git+https://github.com/dclong/dsutil@main
-    
 if [[ $? -eq 0 ]]; then
     cd /opt/python/
     tar -zcvf $file ./
