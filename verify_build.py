9ceec44b6da7d8aae5d4d09f1d9ab6f2a915391e
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-05-12 14:54:17 -0700

    Update verify_build.py

--- verify_build.py
+++ verify_build.py
@@ -3,6 +3,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 EXCLUDE = ("run.sh", "env_pyspark_optimus.sh", )
 
-for path in BASE_DIR.glob("*.sh"):
-    if path.name not in EXCLUDE and not path.with_suffix(".tar.gz").is_file():
-        raise FileNotFoundError(f"{path} was not built successfully!")
+paths = [str(path) for path in BASE_DIR.glob("*.sh") if path.name not in EXCLUDE and not path.with_suffix(".tar.gz").is_file()]
+if paths:
+    raise FileNotFoundError(f"The following environment(s) failed to build:\n{''.join(paths)}")
