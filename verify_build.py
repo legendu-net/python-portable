bac99d173b5cb585a1ec121de41c2b5f905e19ab
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-05-29 09:12:26 -0700

    Update verify_build.py

--- verify_build.py
+++ verify_build.py
@@ -5,4 +5,4 @@ EXCLUDE = ("run.sh", "env_pyspark_optimus.sh", )
 
 paths = [str(path) for path in BASE_DIR.glob("*.sh") if path.name not in EXCLUDE and not path.with_suffix(".tar.gz").is_file()]
 if paths:
-    raise FileNotFoundError(f"The following environment(s) failed to build:\n{''.join(paths)}")
+    raise FileNotFoundError(f"The following environment(s) failed to build:\n{' '.join(paths)}")
