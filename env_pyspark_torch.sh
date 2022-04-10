6b291592b9baa05aa556cb041a8767ff4d686d65
Ben Chuanlong Du <dclong@users.noreply.github.com>
2022-04-10 13:10:17 -0700

    Update env_pyspark_torch.sh

--- env_pyspark_torch.sh
+++ env_pyspark_torch.sh
@@ -5,7 +5,7 @@ file=/workdir/${1:-env_pyspark_torch}.tar.gz
 /opt/python/bin/python3 -m pip install \
     loguru notifiers \
     pyarrow datacompy \
-    torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html \
+    torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
     transformers \
     pysparker
 
