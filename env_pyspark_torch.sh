78d01305b1b1ad2342397d514173f9556ef4a841
Ben Chuanlong Du <dclong@users.noreply.github.com>
2022-04-10 13:08:56 -0700

    Update env_pyspark_torch.sh

--- env_pyspark_torch.sh
+++ env_pyspark_torch.sh
@@ -6,7 +6,8 @@ file=/workdir/${1:-env_pyspark_torch}.tar.gz
     loguru notifiers \
     pyarrow datacompy \
     torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html \
-    transformers
+    transformers \
+    pysparker
 
 if [[ $? -eq 0 ]]; then
     cd /opt/python/
